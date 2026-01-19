2c0a8c4e4edf4d075d6b2d03c815678446f4f080
Damagucci-Juice <jsr9874@gmail.com>
2026-01-19 13:42:14 +0900

    Initial Commit

---
+++ SeSAC_0119/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeSAC_0119
+//
+//  Created by Gucci on 1/19/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
