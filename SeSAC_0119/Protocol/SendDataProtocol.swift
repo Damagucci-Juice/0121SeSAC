372ef7bca7360554e993a3da5d1627bc44a45ddf
Damagucci-Juice <jsr9874@gmail.com>
2026-01-21 15:08:19 +0900

    깃 수정

---
+++ SeSAC_0119/Protocol/SendDataProtocol.swift
@@ -0,0 +1,8 @@
+//
+//  SendDataProtocol.swift
+//  SeSAC_0119
+//
+//  Created by Gucci on 1/21/26.
+//
+
+import Foundation
---
+++ SeSAC_0119/ReusableProtocol.swift
@@ -0,0 +1,8 @@
+//
+//  ReusableProtocol.swift
+//  SeSAC_0119
+//
+//  Created by Gucci on 1/21/26.
+//
+
+import Foundation
